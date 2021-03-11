9d28fd2ea23640ac198006c7a4dc6bf786a74c0f
JOSE DANIEL DANIEL <gon20293@uvg.edu.gt>
2021-03-11 00:39:42 +0000

    Creacion de clase Proceso para utilizarla en la simulacion de procesos.

---
+++ main.py
@@ -0,0 +1,25 @@
+import simpy
+import random
+#import matplotlib.pyplot as plt
+#import statistics as stat
+
+
+class Proceso():
+
+  def __init__(self):
+    self.Ram = 0
+    self.Instrucciones = 0
+
+  def setRam(self,valor):
+    self.Ram=valor
+
+  def getRam(self):
+    return self.Ram
+  
+  def setInst(self, valor):
+    self.Instrucciones=valor
+
+  def getInst(self):
+    return self.Instrucciones
+  
+
